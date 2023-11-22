cea5874f61fe03ad647c17fa288e0d661e61fa8a
Ryan Murphy <ryanmurphy@gatech.edu>
2023-11-21 23:03:02 -0500

    runback charts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -34,3 +34,10 @@ export type Track = keyof typeof trackList;
 export const tracks = Object.keys(trackList) as unknown as Track;
 
 export const percent = (num: number) => (num * 100).toFixed(0) + "%";
+
+const RED = "rgb(255, 71, 24)";
+const BLUE = "rgb(0, 83, 234)";
+const GREEN = "rgb(0, 169, 0)";
+const YELLOW = "rgb(255, 195, 0)";
+export const colors = [RED, BLUE, GREEN, YELLOW];
+export const playerColors = { Ryan: RED, Derek: BLUE, Ben: GREEN, Chris: YELLOW };
--- src/routes/runbacks/[episode]/+page.svelte
+++ src/routes/runbacks/[episode]/+page.svelte
@@ -1,9 +1,87 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+  import { playerColors, trackList } from "$lib";
   import Kart from "$lib/components/Kart.svelte";
   import { msToTime } from "$lib/utils";
+  import Chart from "chart.js/auto";
+  import { onMount } from "svelte";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  let chart: Chart;
+  let ctx: any;
+
+  var order = [
+    "luigiCircuit",
+    "peachBeach",
+    "babyPark",
+    "dryDryDesert",
+    "mushroomBridge",
+    "marioCircuit",
+    "daisyCruiser",
+    "waluigiStadium",
+    "sherbetLand",
+    "mushroomCity",
+    "yoshiCircuit",
+    "dkMountain",
+    "warioColosseum",
+    "dinoDinoJungle",
+    "bowsersCastle",
+    "rainbowRoad",
+  ];
+
+  const timeRecord = data.races
+    .slice(0, Number($page.params.episode))
+    .reduce((best, cur) => ((cur.time ?? Infinity) < (best.time ?? Infinity) ? cur : best));
+
+  const datasets = [data.runback.topScreen, data.runback.bottomScreen].map((runback, i) => {
+    let diff = 0;
+    const times = order.map((track) => {
+      diff += runback.times[track] - timeRecord[track];
+      return diff;
+    });
+    return {
+      label: runback.driver + " + " + runback.items,
+      data: times,
+      fill: false,
+      backgroundColor: playerColors[runback.driver].replace("rgb", "rgba").replace(")", ",.5)"),
+      borderColor: playerColors[runback.driver],
+      pointStyle: false,
+    };
+  });
+
+  onMount(() => {
+    chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: order.map((track) => trackList[track]),
+        datasets,
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        interaction: {
+          intersect: false,
+          mode: "index",
+        },
+        scales: {
+          y: {
+            ticks: {
+              callback: (value) => msToTime(Number(value)),
+            },
+          },
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (context) => context.dataset.label + ": " + msToTime(context.parsed.y),
+            },
+          },
+        },
+      },
+    });
+  });
 </script>
 
 <table class="w-full text-center">
@@ -14,8 +92,8 @@
       <th>Kart</th>
       <th>Time</th>
       <th>Points</th>
-      {#each data.runback.trackOrder as track}
-        <th>{track}</th>
+      {#each order as track}
+        <th>{trackList[track]}</th>
       {/each}
     </tr>
   </thead>
@@ -26,6 +104,15 @@
       <td><Kart kart={data.runback.topScreen.kart} /></td>
       <td>{msToTime(data.runback.topScreen.time)}</td>
       <td>{data.runback.topScreen.points}</td>
+      {#each order as track}
+        <td
+          class={data.runback.topScreen.times[track] > data.runback.bottomScreen.times[track]
+            ? "font-bold bg-primary/20"
+            : undefined}
+        >
+          {msToTime(data.runback.topScreen.times[track])}
+        </td>
+      {/each}
     </tr>
     <tr>
       <td>{data.runback.bottomScreen.driver}</td>
@@ -33,6 +120,18 @@
       <td><Kart kart={data.runback.bottomScreen.kart} /></td>
       <td>{msToTime(data.runback.bottomScreen.time)}</td>
       <td>{data.runback.bottomScreen.points}</td>
+      {#each order as track}
+        <td
+          class={data.runback.bottomScreen.times[track] > data.runback.topScreen.times[track]
+            ? "font-bold bg-primary/20"
+            : undefined}
+        >
+          {msToTime(data.runback.bottomScreen.times[track])}
+        </td>
+      {/each}
     </tr>
   </tbody>
 </table>
+<div class="h- h-[60vh]">
+  <canvas bind:this={ctx} id="chart" />
+</div>
